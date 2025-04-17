de5c4e84b024b70f75c096a31ee2f68a4686d703
abdulazeezsodiq <sodiq.asynctechs@gmail.com>
2025-04-17 14:23:42 +0100

    implemented toast!

--- app/clientProvider.tsx
+++ app/clientProvider.tsx
@@ -1,7 +1,20 @@
 "use client";
-import { store } from "@/redux/store";
+
 import { Provider } from "react-redux";
+import dynamic from "next/dynamic";
+import { store } from "@/redux/store";
+
+// Dynamically import ToastContainer (no SSR)
+const ToastContainer = dynamic(
+  () => import("react-toastify").then((mod) => mod.ToastContainer),
+  { ssr: false }
+);
 
 export function ClientProviders({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      {children}
+      <ToastContainer />
+    </Provider>
+  );
 }
